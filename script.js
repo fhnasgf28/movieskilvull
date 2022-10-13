edc807203be428710200beb719404b27cd907d4a
1910631170019_FarhanAssegaf <82705668+fhnasgf28@users.noreply.github.com>
2022-10-13 21:16:35 +0700

    menambahkan bootstrap ke dalam projek

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 $.ajax({
-    url: 'https://api.themoviedb.org/3/movie/550?api_key=5c866126da07e4b15b71ac6b675b6995',
+    url: 'https://www.omdbapi.com/?apikey=dca61bcc&i=',
     success: result => {
         const movies = result.Search;
         let cards = '';
         movies.forEach(m => {
             cards += `<div class="col-md-4 my-3">
             <div class="card">
-                <img src="${m.poster}" class="card-img-top" >
+                <img src="${m.Poster}" class="card-img-top" >
                 <div class="card-body">
                   <h5 class="card-title">${m.Title}</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">${m.year}</h6>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                  <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+                    <a href="#" class="btn btn-primary modal-detail-button data-imdb=${m.imdbId}data-bs-toggle="modal" data-bs-target="#modalmovie ">Show Detail</a>
                 </div>
               </div>
         </div>`;
     });
     $(".movies-container").html(cards);
+
+    // ketika tombol detail di klik
+    $(".modal-detail-button").on(`click`,function(){
+        $.aja
+    })
     },
 
     error: (e) => {
